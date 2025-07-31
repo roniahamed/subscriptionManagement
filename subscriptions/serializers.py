98d835e396dd670328217662a3b074ddec469e07
roniahamed <mdroniahamed56@gmail.com>
2025-08-01 00:47:42 +0600

    subscriptionSerializer

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -6,3 +6,16 @@ class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Subscription
+        fields = ('id', 'user', 'plan', 'start_date', 'end_date', 'status')
