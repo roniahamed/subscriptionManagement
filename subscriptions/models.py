529aa73d59fba0865a47b4c12b2b32c553f42977
roniahamed <mdroniahamed56@gmail.com>
2025-07-31 21:14:21 +0600

    Plan

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+
+class Plan(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Price in USD")
+    duration_days = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} - ${self.price} for {self.duration_days} days"
