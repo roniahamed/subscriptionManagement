766448eb3b65bd9733fc33d96009cc2c891c8cac
roniahamed <mdroniahamed56@gmail.com>
2025-07-31 21:40:47 +0600

    adding Subscription  admin

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -5,3 +5,10 @@ from .models import Plan, Subscription, ExchangeRateLog
 class PlanAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'duration_days')
     search_fields = ('name',)
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'start_date', 'end_date', 'status')
+    list_filter = ('status', 'plan')
+    search_fields = ('user__username',)
+    autocomplete_fields = ('user', 'plan')
