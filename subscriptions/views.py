06421c960dbfb876f5951c0e384e9cbf2515248d
roniahamed <mdroniahamed56@gmail.com>
2025-08-02 01:45:07 +0600

    ading requirements.txt

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,8 @@
+import requests
+from django.conf import settings
+from django.db import transaction
 from django.shortcuts import render
-
-# Create your views here.
+from django.views import View
+from rest_framework import generics, views, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
